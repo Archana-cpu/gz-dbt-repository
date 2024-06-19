d2723ea3c05c758b38febc9a02f59316e6ac2efa
archanagajula27@gmail.com <archanagajula27@gmail.com>
2024-06-19 10:47:02 +0000

    changed the main table to orders, it was sales by mistake

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -3,13 +3,12 @@ select
     orders_id,
     revenue, 
     quantity, 
-    purchase_price,
     purchase_cost,
     margin,
     shipping_fee,
     logcost,
     ship_cost,
     ROUND(sm.margin + sh.shipping_fee - (sh.logcost + sh.ship_cost),2) AS operational_margin 
-from {{ ref('int_sales_margin') }} sm
+from {{ ref('int_orders_margin') }} sm
 Join {{ ref('stg_raw__ship') }} sh
     using(orders_id)
