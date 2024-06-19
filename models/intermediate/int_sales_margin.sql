09d8cc719e12bda1249ade1c9ab3cb7de4fe579f
archanagajula27@gmail.com <archanagajula27@gmail.com>
2024-06-19 10:42:36 +0000

    removed casting

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -5,8 +5,8 @@ select
     revenue, 
     quantity, 
     purchase_price,
-    ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS purchase_cost,
-    ROUND(s.revenue - ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2)) AS margin
+    ROUND(s.quantity*purchase_price),2 AS purchase_cost,
+    ROUND(s.revenue - ROUND(s.quantity*p.purchase_price),2) AS margin
 from {{ ref('stg_raw__sales') }} s
 Join {{ ref('stg_raw__product') }} p
     using(products_id)
