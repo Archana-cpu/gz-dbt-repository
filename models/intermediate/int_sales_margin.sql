7fc76febf87969f54f21949a72b7c94bd3ad5cc6
archanagajula27@gmail.com <archanagajula27@gmail.com>
2024-06-19 13:00:12 +0000

    end of challenge 1

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -4,9 +4,9 @@ select
     orders_id,
     revenue, 
     quantity, 
-    purchase_price,
-    ROUND(s.quantity*purchase_price),2 AS purchase_cost,
-    ROUND(s.revenue - ROUND(s.quantity*p.purchase_price),2) AS margin
+    purchase_price, 
+    ROUND(s.quantity* p.purchase_price ,2) AS purchase_cost,
+    s.revenue - ROUND(s.quantity * p.purchase_price,2) AS margin
 from {{ ref('stg_raw__sales') }} s
 Join {{ ref('stg_raw__product') }} p
     using(products_id)
