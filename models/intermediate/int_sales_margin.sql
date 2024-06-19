b03cd9d587f4f53e51c4304756f7500590e230a9
archanagajula27@gmail.com <archanagajula27@gmail.com>
2024-06-19 09:36:12 +0000

    orders margin created

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -4,9 +4,9 @@ select
     orders_id,
     revenue, 
     quantity, 
-    CAST(purchase_price AS FLOAT64), 
+    purchase_price,
     ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS purchase_cost,
-    s.revenue - ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS margin
+    ROUND(s.revenue - ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2)) AS margin
 from {{ ref('stg_raw__sales') }} s
 Join {{ ref('stg_raw__product') }} p
     using(products_id)
